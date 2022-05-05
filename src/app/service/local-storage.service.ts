3cd5c2e868822f4f42ea12c8aa60d2dd712c746e
Enerida Lama <eneridalamaa@gmail.com>
2022-05-06 00:08:30 +0200

    added local storage

--- src/app/service/local-storage.service.ts
+++ src/app/service/local-storage.service.ts
@@ -1,21 +1,55 @@
 import { Injectable } from '@angular/core';
+
+interface Event {
+  id: number;
+  title: string;
+  start: string;
+  end: string;
+  description?: string;
+}
+
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class LocalStorageService {
-  constructor() { }
+  constructor() {}
 
-  public setItem(key: string, value: string) {
-    localStorage.setItem(key, value);
+  getEventList(): Event[] {
+    return localStorage.getItem('events')
+      ? JSON.parse(localStorage.getItem('events') as string)
+      : [];
   }
-    
-  public getItem(key: string){ 
-    return localStorage.getItem(key)
+
+  addEvent(event: Omit<Event, 'id'>): any[] {
+    const events: Event[] = this.getEventList();
+    let id: number;
+    if (events && events.length) {
+      const lastEvent = events.pop() as Event;
+      id = lastEvent.id + 1;
+      events.push(lastEvent);
+    } else {
+      id = 1;
+    }
+    events.push({ ...event, id });
+    localStorage.setItem('events', JSON.stringify(events));
+    return events;
   }
-  // public removeItem(key:string) {
-  //   localStorage.removeItem(key);
+
+  getEventById(id: number): Event | undefined {
+    const events: Event[] = this.getEventList();
+    return events.find((event) => event.id === id);
+  }
+
+  // public setItem(key: string, value: string) {
+  //   localStorage.setItem(key, value);
   // }
-  // public clear(){
-  //   localStorage.clear(); 
-  // }
+
+  // public getItem(key: string) {
+  //   return localStorage.getItem(key);
 }
+// public removeItem(key:string) {
+//   localStorage.removeItem(key);
+// }
+// public clear(){
+//   localStorage.clear();
+// }
--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -7,66 +7,65 @@ import { LocalStorageService } from '../service/local-storage.service';
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.scss']
+  styleUrls: ['./timeline.component.scss'],
 })
 export class TimelineComponent implements OnInit {
   subscription: Subscription = new Subscription();
 
-  calendarOptions: CalendarOptions = {
-    initialView: 'timeGridDay',
-    customButtons: {},
-    progressiveEventRendering: true,
-    height: 600,
-    expandRows: true,
-    handleWindowResize: true,
-    nowIndicator: true,
-    eventColor: 'rgb(141,208,255)',
-    eventTextColor: 'rgb(42,50,61)',
-    // slotMinTime: '09:00:00',
-    // slotMaxTime: '21:00:00',
-    titleFormat: {
-      year: 'numeric',
-      month: 'short',
-      day: '2-digit',
-    },
-    headerToolbar: {
-      start: 'today',
-      center: 'title',
-      end: 'prev,next',
-    },
-    eventOverlap: false,
-    editable: true,
-    eventStartEditable: true,
-    eventResizableFromStart: true,
-    eventDurationEditable: true,
-   
-    events: []
-  };
-  
-  constructor(private commonService: CommonService, private localStorageService: LocalStorageService) {}
+  calendarOptions: CalendarOptions = {};
+
+  constructor(
+    private commonService: CommonService,
+    private localStorageService: LocalStorageService
+  ) {
+    this.calendarOptions = {
+      initialView: 'timeGridDay',
+      customButtons: {},
+      progressiveEventRendering: true,
+      height: 600,
+      expandRows: true,
+      handleWindowResize: true,
+      nowIndicator: true,
+      eventColor: 'rgb(141,208,255)',
+      eventTextColor: 'rgb(42,50,61)',
+      // slotMinTime: '09:00:00',
+      // slotMaxTime: '21:00:00',
+      titleFormat: {
+        year: 'numeric',
+        month: 'short',
+        day: '2-digit',
+      },
+      headerToolbar: {
+        start: 'today',
+        center: 'title',
+        end: 'prev,next',
+      },
+      eventOverlap: false,
+      editable: true,
+      eventStartEditable: true,
+      eventResizableFromStart: true,
+      eventDurationEditable: true,
+      events: localStorageService.getEventList() as any,
+      eventClick: function(calEvent){console.log('calEvent ',calEvent.event.remove())}
+    };
+  }
 
   ngOnInit(): void {
-
-    this.subscription = this.commonService.data.subscribe((val) => {  
-      this.calendarOptions.events = [
-        ...this.calendarOptions.events as any,{
-          id: val.firstname,
-          title: val.firstname + ' ' + val.lastname+ ' - ' + val.number,
-          start: val.date + 'T' + val.hour,
-          end: val.date + 'T' + val.duration, 
-        },
-        
-      ];
-      this.localStorageService.getItem(JSON.stringify(this.calendarOptions.events))
+    this.subscription = this.commonService.data.subscribe((val) => {
+      this.calendarOptions.events = this.localStorageService.addEvent({
+        title: val.firstname + ' ' + val.lastname + ' - ' + val.number,
+        start: val.date + 'T' + val.hour,
+        end: val.date + 'T' + val.duration,
+        description: 'test'
+      });
     });
-    console.log(this.calendarOptions.events)
+
+    console.log(this.localStorageService.getEventById(1))
   }
-  
 
   ngOnDestroy() {
     if (this.subscription) {
       this.subscription.unsubscribe();
     }
   }
-
 }
